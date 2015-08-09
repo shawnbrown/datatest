152ca687461312a44033ba8a507ff7df1dbd66e6
Shawn Brown <shawnbrown@users.noreply.github.com>
2015-08-09 13:45:55 -0700

    Move UnicodeCsvReader to separate file to clean-up datasource module.

---
+++ datatest/_unicodecsvreader.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""Unicode CSV Reader (Python 3 and Python 2 compatible)."""
+
+import csv
+import io
+import sys
+
+
+class UnicodeCsvReader:
+    """UnicodeCsvReader wraps the standard library's ``csv.reader``
+    object to support unicode CSV files in both Python 3 and Python 2.
+
+    Example usage::
+
+        with UnicodeCsvReader('myfile.csv', encoding='utf-8') as reader:
+            for row in reader:
+                process(row)
+
+    The *csvfile* argument can be a file path (as in the example above)
+    or a file-like object.  When passing file objects, Python 3 requires
+    them to be opened in text-mode ('r') while Python 2 requires them to
+    be opened in binary-mode ('rb').  UnicodeCsvReader manages these
+    differences automatically when given a file path.
+
+    """
+
+    def __init__(self, csvfile, encoding='utf-8', dialect='excel', **fmtparams):
+        self.encoding = encoding
+        self.dialect = dialect
+        self._csvfile = csvfile  # Can be path or file-like object.
+        self._fileobj = self._get_file_object(csvfile, self.encoding)
+        self._reader = csv.reader(self._fileobj, dialect=self.dialect, **fmtparams)
+
+    @property
+    def line_num(self):
+        return self._reader.line_num
+
+    def __del__(self):
+        # Note: If __init__ fails, _fileobj will not exist.
+        if hasattr(self, '_fileobj') and self._fileobj != self._csvfile:
+            self._fileobj.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.__del__()
+
+    def __iter__(self):
+        return self
+
+    @staticmethod
+    def _get_file_object(csvfile, encoding):
+        if isinstance(csvfile, str):
+            return open(csvfile, 'rt', encoding=encoding, newline='')  # <- EXIT!
+
+        if hasattr(csvfile, 'mode'):
+            assert 'b' not in csvfile.mode, "File must be open in text mode ('rt')."
+        elif issubclass(csvfile.__class__, io.IOBase):
+            assert issubclass(csvfile.__class__, io.TextIOBase), ("Stream object must inherit "
+                                                                  "from io.TextIOBase.")
+        return csvfile
+
+    def __next__(self):
+        return next(self._reader)
+
+
+########################################################################
+# Patch `UnicodeCsvReader` if using Python 2.
+########################################################################
+if sys.version < '3':
+    _py3_UnicodeCsvReader = UnicodeCsvReader
+    class UnicodeCsvReader(_py3_UnicodeCsvReader):
+        @staticmethod
+        def _get_file_object(csvfile, encoding):
+            if isinstance(csvfile, str):
+                return open(csvfile, 'rb')  # <- EXIT!
+
+            if hasattr(csvfile, 'mode'):
+                assert 'b' in csvfile.mode, ("When using Python 2, file must "
+                                             "be open in binary mode ('rb').")
+            elif issubclass(csvfile.__class__, io.IOBase):
+                assert not issubclass(csvfile.__class__, io.TextIOBase), ("When using Python 2, "
+                                                                          "must use byte stream "
+                                                                          "(not text stream).")
+            return csvfile
+
+        def __next__(self):
+            row = next(self._reader)
+            return [s.decode(self.encoding) for s in row]
+
+        def next(self):
+            return self.__next__()
--- datatest/datasource.py
+++ datatest/datasource.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
 import collections
-import csv
 import inspect
-import io
 import os
 import sqlite3
-import sys
 import warnings
 from decimal import Decimal
 
 from datatest._builtins import *
+from datatest._unicodecsvreader import UnicodeCsvReader as _UnicodeCsvReader
 import datatest._itertools as itertools
 
 #pattern = 'test*.py'
@@ -189,91 +187,6 @@ class SqliteDataSource(BaseDataSource):
 
         clause = ' AND '.join(clause) if clause else ''
         return clause, params
-
-
-class _UnicodeCsvReader:
-    """UnicodeCsvReader wraps the standard library's ``csv.reader``
-    object to support unicode CSV files in both Python 3 and Python 2.
-
-    Example usage:
-
-        with UnicodeCsvReader('myfile.csv', encoding='utf-8') as reader:
-            for row in reader:
-                process(row)
-
-    The *csvfile* argument can be a file path (as in the example above)
-    or a file-like object.  When passing file objects, Python 3 requires
-    them to be opened in text-mode ('r') while Python 2 requires them to
-    be opened in binary-mode ('rb').  UnicodeCsvReader manages these
-    differences automatically when given a file path.
-
-    """
-
-    def __init__(self, csvfile, encoding='utf-8', dialect='excel', **fmtparams):
-        self.encoding = encoding
-        self.dialect = dialect
-        self._csvfile = csvfile  # Can be path or file-like object.
-        self._fileobj = self._get_file_object(csvfile, self.encoding)
-        self._reader = csv.reader(self._fileobj, dialect=self.dialect, **fmtparams)
-
-    @property
-    def line_num(self):
-        return self._reader.line_num
-
-    def __del__(self):
-        # Note: When  __init__ fails, _fileobj will not exist.
-        if hasattr(self, '_fileobj') and self._fileobj != self._csvfile:
-            self._fileobj.close()
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.__del__()
-
-    def __iter__(self):
-        return self
-
-    @staticmethod
-    def _get_file_object(csvfile, encoding):
-        if isinstance(csvfile, str):
-            return open(csvfile, 'rt', encoding=encoding, newline='')  # <- EXIT!
-
-        if hasattr(csvfile, 'mode'):
-            assert 'b' not in csvfile.mode, "File must be open in text mode ('rt')."
-        elif issubclass(csvfile.__class__, io.IOBase):
-            assert issubclass(csvfile.__class__, io.TextIOBase), ("Stream object must inherit "
-                                                                  "from io.TextIOBase.")
-        return csvfile
-
-    def __next__(self):
-        return next(self._reader)
-
-
-# Patch `_UnicodeCsvReader` if using Python 2.
-if sys.version < '3':
-    _py3_UnicodeCsvReader = _UnicodeCsvReader
-    class _UnicodeCsvReader(_py3_UnicodeCsvReader):
-        @staticmethod
-        def _get_file_object(csvfile, encoding):
-            if isinstance(csvfile, str):
-                return open(csvfile, 'rb')  # <- EXIT!
-
-            if hasattr(csvfile, 'mode'):
-                assert 'b' in csvfile.mode, ("When using Python 2, file must "
-                                             "be open in binary mode ('rb').")
-            elif issubclass(csvfile.__class__, io.IOBase):
-                assert not issubclass(csvfile.__class__, io.TextIOBase), ("When using Python 2, "
-                                                                          "must use byte stream "
-                                                                          "(not text stream).")
-            return csvfile
-
-        def __next__(self):
-            row = next(self._reader)
-            return [s.decode(self.encoding) for s in row]
-
-        def next(self):
-            return self.__next__()
 
 
 class CsvDataSource(SqliteDataSource):
