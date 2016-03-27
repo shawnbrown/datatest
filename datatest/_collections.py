5d17af4871e6125d6ac6d9bb7d453bda74e5c4ae
Shawn Brown <shawnbrown@users.noreply.github.com>
2016-03-27 03:39:12 -0400

    Add full implementation of namedtuple for Python 2.6 compatibility--the previous patch did not adequately implement the *rename* argument.

--- datatest/_collections.py
+++ datatest/_collections.py
@@ -6,27 +6,100 @@ from collections import *
 try:
     namedtuple('x', ['x'], rename=True)  # Argument 'rename' new in 2.7
 except TypeError:
-    # Code adapted from the Python 2.7 standard library.
-    namedtuple_orig = namedtuple
+    # Copyright 2007 Raymond Hettinger
+    # From http://code.activestate.com/recipes/500261-named-tuples/
+    # Released under the Python Software Foundation License
+    # <http://www.opensource.org/licenses/Python-2.0>.
+    from operator import itemgetter as _itemgetter
     from keyword import iskeyword as _iskeyword
+    import sys as _sys
+
+    namedtuple_orig = namedtuple
+
     def namedtuple(typename, field_names, verbose=False, rename=False):
+        # Parse and validate the field names.  Validation serves two purposes,
+        # generating informative error messages and preventing template injection attacks.
         if isinstance(field_names, basestring):
-            field_names = field_names.replace(',', ' ').split()
-        field_names = map(str, field_names)
-        typename = str(typename)
+            field_names = field_names.replace(',', ' ').split() # names separated by whitespace and/or commas
+        #field_names = tuple(map(str, field_names))  # <- Bad recursion behavior in Python 2.5
+        field_names = tuple([str(x) for x in field_names])
         if rename:
+            names = list(field_names)
             seen = set()
-            for index, name in enumerate(field_names):
-                if (not all(c.isalnum() or c=='_' for c in name)
-                    or _iskeyword(name)
-                    or not name
-                    or name[0].isdigit()
-                    or name.startswith('_')
+            for i, name in enumerate(names):
+                if (not min(c.isalnum() or c=='_' for c in name) or _iskeyword(name)
+                    or not name or name[0].isdigit() or name.startswith('_')
                     or name in seen):
-                    field_names[index] = '_%d' % index
+                        names[i] = '_%d' % i
                 seen.add(name)
-        return namedtuple_orig(typename, field_names, verbose)
-    namedtuple.__doc__ == namedtuple_orig.__doc__
+            field_names = tuple(names)
+        for name in (typename,) + field_names:
+            if not min(c.isalnum() or c=='_' for c in name):
+                raise ValueError('Type names and field names can only contain alphanumeric characters and underscores: %r' % name)
+            if _iskeyword(name):
+                raise ValueError('Type names and field names cannot be a keyword: %r' % name)
+            if name[0].isdigit():
+                raise ValueError('Type names and field names cannot start with a number: %r' % name)
+        seen_names = set()
+        for name in field_names:
+            if name.startswith('_') and not rename:
+                raise ValueError('Field names cannot start with an underscore: %r' % name)
+            if name in seen_names:
+                raise ValueError('Encountered duplicate field name: %r' % name)
+            seen_names.add(name)
+        # Create and fill-in the class template
+        numfields = len(field_names)
+        argtxt = repr(field_names).replace("'", "")[1:-1]   # tuple repr without parens or quotes
+        reprtxt = ', '.join('%s=%%r' % name for name in field_names)
+        template = '''class %(typename)s(tuple):
+            '%(typename)s(%(argtxt)s)' \n
+            __slots__ = () \n
+            _fields = %(field_names)r \n
+            def __new__(_cls, %(argtxt)s):
+                return _tuple.__new__(_cls, (%(argtxt)s)) \n
+            @classmethod
+            def _make(cls, iterable, new=tuple.__new__, len=len):
+                'Make a new %(typename)s object from a sequence or iterable'
+                result = new(cls, iterable)
+                if len(result) != %(numfields)d:
+                    raise TypeError('Expected %(numfields)d arguments, got %%d' %% len(result))
+                return result \n
+            def __repr__(self):
+                return '%(typename)s(%(reprtxt)s)' %% self \n
+            def _asdict(self):
+                'Return a new dict which maps field names to their values'
+                return dict(zip(self._fields, self)) \n
+            def _replace(_self, **kwds):
+                'Return a new %(typename)s object replacing specified fields with new values'
+                result = _self._make(map(kwds.pop, %(field_names)r, _self))
+                if kwds:
+                    raise ValueError('Got unexpected field names: %%r' %% kwds.keys())
+                return result \n
+            def __getnewargs__(self):
+                return tuple(self) \n\n''' % locals()
+        for i, name in enumerate(field_names):
+            template += '            %s = _property(_itemgetter(%d))\n' % (name, i)
+        if verbose:
+            print(template)
+        # Execute the template string in a temporary namespace
+        namespace = dict(_itemgetter=_itemgetter, __name__='namedtuple_%s' % typename,
+                         _property=property, _tuple=tuple)
+        try:
+            exec(template, namespace)
+        except SyntaxError as e:
+            raise SyntaxError(str(e) + ':\n' + template)
+        result = namespace[typename]
+        # For pickling to work, the __module__ variable needs to be set to the frame
+        # where the named tuple is created.  Bypass this step in enviroments where
+        # sys._getframe is not defined (Jython for example) or sys._getframe is not
+        # defined for arguments greater than 0 (IronPython).
+        try:
+            result.__module__ = _sys._getframe(1).f_globals.get('__name__', '__main__')
+        except (AttributeError, ValueError):
+            pass
+        return result
+
+    namedtuple.__doc__ = namedtuple_orig.__doc__
 
 
 try:
