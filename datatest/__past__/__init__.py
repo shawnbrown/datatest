0191df6e2a50e84e1f11bdfbd6a072ef871e4412
Shawn Brown <shawnbrown@users.noreply.github.com>
2016-04-21 00:30:17 -0400

    Add docstrings to '__past__' submodule to explain backwards compatibility scheme.

--- datatest/__past__/__init__.py
+++ datatest/__past__/__init__.py
@@ -1,1 +1,13 @@
-#
+# -*- coding: utf-8 -*-
+"""Backwards compatibility for phased-out features and behaviors.
+
+To use a feature that is no longer supported in the current version of
+datatest, use the following:
+
+    from datatest.__past__ import <feature-name>_<major-version>
+
+For example, importing 'assertions_alpha' would provide backwards
+compatibility for the assertion methods that were implemented in the
+alpha/pre-release version of datatest.
+
+"""
--- datatest/__past__/allowances_alpha.py
+++ datatest/__past__/allowances_alpha.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Backwards compatibility for allowance methods from alpha version."""
 from __future__ import absolute_import
 from datatest import DataTestCase
 
