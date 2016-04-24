928fe955b65480cf48bfd5dbb53c39c0b55751e5
Shawn Brown <shawnbrown@users.noreply.github.com>
2016-04-24 02:34:56 -0400

    Remove unused imports and references.

--- tests/test_sources_pandas.py
+++ tests/test_sources_pandas.py
@@ -1,18 +1,11 @@
-
+# -*- coding: utf-8 -*-
 from . import _unittest as unittest
-from .test_sources_base import MinimalSource
-
 from .mixins import CountTests
 from .mixins import OtherTests
 
-from datatest import ExcelSource
 from datatest import PandasSource
 from datatest.sources.pandas import _version_info
 
-try:
-    import xlrd
-except ImportError:
-    xlrd = None
 
 try:
     import pandas
@@ -20,13 +13,6 @@ try:
                 and _version_info(pandas.np) >= (1, 7, 1))
 except (ImportError, AssertionError):
     pandas = None
-
-
-#@unittest.skipIf(xlrd is None, 'xlrd not installed')
-#class TestExcelSource(unittest.TestCase, TestAll):
-#    def setUp(self):
-#        path =
-#        self.datasource = ExcelSource(path)
 
 
 @unittest.skipIf(pandas is None, 'pandas not found')
