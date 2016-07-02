5f4daa9421cf17b85c029a066518ed89546663ca
Shawn Brown <shawnbrown@users.noreply.github.com>
2016-07-02 19:24:50 -0400

    Add wrapper for _find_data_source() method to find both new and old data source names when using backwards compatibility modules.

--- datatest/__past__/api_dev0.py
+++ datatest/__past__/api_dev0.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 """Backwards compatibility for version 0.6.0.dev0 API."""
 from __future__ import absolute_import
-from datatest import DataTestCase
-
-DataTestCase.subjectData = DataTestCase.subject
-DataTestCase.referenceData = DataTestCase.reference
+from datatest.__past__ import api_dev1
--- datatest/__past__/api_dev1.py
+++ datatest/__past__/api_dev1.py
@@ -3,6 +3,20 @@
 from __future__ import absolute_import
 from datatest import DataTestCase
 
-
 DataTestCase.subjectData = DataTestCase.subject
 DataTestCase.referenceData = DataTestCase.reference
+
+_wrapped_find_data_source = DataTestCase._find_data_source
+def _find_data_source(name):
+    if name == 'subject':
+        try:
+            return _wrapped_find_data_source('subject')
+        except NameError:
+            return _wrapped_find_data_source('subjectData')
+    elif name == 'reference':
+        try:
+            return _wrapped_find_data_source('reference')
+        except NameError:
+            return _wrapped_find_data_source('referenceData')
+    return _wrapped_find_data_source(name)
+DataTestCase._find_data_source = staticmethod(_find_data_source)
