b577b374bfde6d23aae6211e657204e009a43b41
Shawn Brown <shawnbrown@users.noreply.github.com>
2016-07-09 12:34:25 -0400

    Change backwards compatibility functions (_find_data_source and _normalize_required) to fix bug when accessing data sources using the old subjectData and referenceData names.

--- datatest/__past__/api_dev1.py
+++ datatest/__past__/api_dev1.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 """Backwards compatibility for version 0.6.0.dev1 API."""
 from __future__ import absolute_import
+import inspect
 import datatest
 from datatest import DataTestCase
 
-DataTestCase.subjectData = DataTestCase.subject
-DataTestCase.referenceData = DataTestCase.reference
+
+DataTestCase.subjectData = property(fget=DataTestCase.subject.fget,
+                                    fset=DataTestCase.subject.fset)
+DataTestCase.referenceData = property(fget=DataTestCase.reference.fget,
+                                      fset=DataTestCase.reference.fset)
 DataTestCase.assertDataColumns = DataTestCase.assertSubjectColumns
 DataTestCase.assertDataSet = DataTestCase.assertSubjectSet
 DataTestCase.assertDataSum = DataTestCase.assertSubjectSum
@@ -17,18 +21,38 @@ datatest.DataAssertionError = datatest.DataError
 _wrapped_find_data_source = DataTestCase._find_data_source
 @staticmethod
 def _find_data_source(name):
-    if name == 'subject':
-        try:
-            return _wrapped_find_data_source('subject')
-        except NameError:
-            return _wrapped_find_data_source('subjectData')
-    elif name == 'reference':
-        try:
-            return _wrapped_find_data_source('reference')
-        except NameError:
-            return _wrapped_find_data_source('referenceData')
+    if name in ('subject', 'subjectData'):
+        stack = inspect.stack()
+        stack.pop()  # Skip record of current frame.
+        for record in stack:
+            frame = record[0]
+            if 'subject' in frame.f_globals:
+                return frame.f_globals['subject']  # <- EXIT!
+            if 'subjectData' in frame.f_globals:
+                return frame.f_globals['subjectData']  # <- EXIT!
+        raise NameError('cannot find {0!r}'.format(name))
+    elif name in ('reference', 'referenceData'):
+        stack = inspect.stack()
+        stack.pop()  # Skip record of current frame.
+        for record in stack:
+            frame = record[0]
+            if 'reference' in frame.f_globals:
+                return frame.f_globals['reference']  # <- EXIT!
+            if 'referenceData' in frame.f_globals:
+                return frame.f_globals['referenceData']  # <- EXIT!
+        raise NameError('cannot find {0!r}'.format(name))
     return _wrapped_find_data_source(name)
 DataTestCase._find_data_source = _find_data_source
+
+
+def _normalize_required(self, required, method, *args, **kwds):
+    if required == None:
+        required = self.referenceData  # <- OLD NAME!
+    if isinstance(required, datatest.BaseSource):
+        fn = getattr(required, method)
+        required = fn(*args, **kwds)
+    return required
+DataTestCase._normalize_required = _normalize_required
 
 
 def _assertDataCount(self, column, keys, required=None, msg=None, **kwds_filter):
