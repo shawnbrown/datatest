7f518ce727d3bb74cbcb50168e52e67d41560510
Shawn Brown <shawnbrown@users.noreply.github.com>
2016-09-17 11:28:11 -0400

    Add additional backward compatibility test for assertEqual() behavior.  The previous test only checked for the wrapper's existence not its behavior.

--- tests/past_api_dev2.py
+++ tests/past_api_dev2.py
@@ -6,19 +6,53 @@ development-release API.
           ``datatest`` package, these tests should be run in a separate
           process.
 """
+from . import _io as io
 from . import _unittest as unittest
 import datatest
 from datatest.__past__ import api_dev2  # <- MONKEY PATCH!!!
 
 
-class TestApiDev2(unittest.TestCase):
-    def test_assertEqual_wrapper(self):
+class TestNamesAndAttributes(unittest.TestCase):
+    def _run_wrapped_test(self, case, method):
+        audit_case = case(method)
+        runner = unittest.TextTestRunner(stream=io.StringIO())
+        result = runner.run(audit_case)
+
+        error = result.errors[0][1] if result.errors else None
+        failure = result.failures[0][1] if result.failures else None
+        return error, failure
+
+    def test_names(self):
         """In the 0.7.0 API, the assertEqual() method should be wrapped
         in a datatest.DataTestCase method of the same name.
         """
+        # TODO: Add this check once the class has been renamed.
+        # Check for DataTestCase name (now TestCase).
+        #self.assertTrue(hasattr(datatest, 'DataTestCase'))
+
+        # Check that wrapper exists.
         datatest_eq = datatest.DataTestCase.assertEqual
         unittest_eq = unittest.TestCase.assertEqual
         self.assertIsNot(datatest_eq, unittest_eq)
+
+    def test_assertEqual(self):
+        """Test for 0.7.0 assertEqual() wrapper behavior."""
+        class _TestWrapper(datatest.DataTestCase):
+            def test_method(_self):
+                first = set([1, 2, 3])
+                second = set([1, 2, 3, 4])
+                with self.assertRaises(datatest.DataError) as cm:
+                    _self.assertEqual(first, second)  # <- Wrapped method!
+
+                msg = 'In 0.7.0, assertEqual() should raise DataError.'
+                _self.assertTrue(isinstance(cm.exception, datatest.DataError), msg)
+
+                diffs = list(cm.exception.differences)
+                _self.assertEqual(diffs, [datatest.Missing(4)])
+
+        error, failure = self._run_wrapped_test(_TestWrapper, 'test_method')
+        self.assertIsNone(error)
+        self.assertIsNone(failure)
 
 
 if __name__ == '__main__':
