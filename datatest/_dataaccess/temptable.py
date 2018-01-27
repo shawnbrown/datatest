aabb7bb668b18a739a97db03337bfde18afba32f
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-01-27 16:05:19 -0500

    Add temptable, associated tests, and sample data (code repurposed from personal project).

---
+++ datatest/_dataaccess/temptable.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+from collections import Iterable
+from collections import Mapping
+from itertools import count
+from itertools import chain
+
+
+try:
+    string_types = basestring
+except NameError:
+    string_types = str
+
+
+def table_exists(cursor, table):
+    cursor.execute('''
+        SELECT name
+        FROM sqlite_master
+        WHERE type='table' AND name=?
+
+        UNION
+
+        SELECT name
+        FROM sqlite_temp_master
+        WHERE type='table' AND name=?
+    ''', (table, table))
+    return bool(cursor.fetchall())
+
+
+_table_names = ('tbl{0}'.format(x) for x in count())
+def new_table_name(cursor):
+    global _table_names
+
+    new_name = next(_table_names)
+    while table_exists(cursor, new_name):
+        new_name = next(_table_names)
+
+    return new_name
+
+
+def normalize_names(names):
+    def normalize(name):
+        name = str(name).strip()  # Strip whitespace.
+        return '"{0}"'.format(name.replace('"', '""'))  # Escape quotes.
+
+    if isinstance(names, string_types) or not isinstance(names, Iterable):
+        return normalize(names)
+    return [normalize(name) for name in names]
+
+
+def create_table(cursor, table, columns, default="''"):
+    """Creates a temporary table using *table* and *columns* names."""
+    columns = normalize_names(columns)
+    if columns.count('""') > 1:
+        custom_message = ('duplicate column name: contains multiple '
+                          'columns where names are empty strings or '
+                          'whitespace')
+        raise sqlite3.OperationalError(custom_message)
+        # Above: The default language for this corner case is very
+        # confusing. Catching this condition and raising the error
+        # before execution is simpler than parsing the inevitable
+        # OperationalError and re-raising it with a modified message.
+
+    if not default:
+        default = 'NULL'
+    column_defs = ['{0} DEFAULT {1}'.format(x, default) for x in columns]
+    column_defs = ', '.join(column_defs)
+
+    statement = 'CREATE TEMPORARY TABLE {0} ({1})'.format(table, column_defs)
+    cursor.execute(statement)
+
+
+def get_columns(cursor, table):
+    """Returns list of column names used in table."""
+    cursor.execute('PRAGMA table_info({0})'.format(table))
+    columns = [x[1] for x in cursor]
+    if not columns:
+        raise sqlite3.ProgrammingError('no such table: {0}'.format(table))
+    return columns
+
+
+def insert_records(cursor, table, columns, records):
+    table = normalize_names(table)
+    columns = normalize_names(columns)
+    sql = 'INSERT INTO {0} ({1}) VALUES ({2})'.format(
+        table,
+        ', '.join(columns),
+        ', '.join(['?'] * len(columns)),
+    )
+    try:
+        cursor.executemany(sql, records)
+    except sqlite3.ProgrammingError as error:
+        if 'incorrect number of bindings' in str(error).lower():
+            msg = (
+                '{0}\n\nThe records {1!r} contains some rows with too '
+                'few or too many values. Before loading this data, it '
+                'must be normalized so each row contains a number of '
+                'values equal to the number of columns being loaded.'
+            ).format(error, records)
+            error = sqlite3.ProgrammingError(msg)
+            error.__cause__ = None
+        raise error
+
+
+def alter_table(cursor, table, columns, default="''"):
+    existing_columns = set(normalize_names(get_columns(cursor, table)))
+    for column in normalize_names(columns):
+        if column in existing_columns:
+            continue
+
+        if not default:
+            default = 'NULL'
+        sql = 'ALTER TABLE {0} ADD COLUMN {1} DEFAULT {2}'
+        sql = sql.format(table, column, default)
+
+        cursor.execute(sql)
+        existing_columns.add(column)
+
+
+def drop_table(cursor, table):
+    cursor.execute('DROP TABLE IF EXISTS {0}'.format(table))
+
+
+_savepoint_names = ('svpnt{0}'.format(x) for x in count())
+class savepoint(object):
+    """Sqlite SAVEPOINT context manager."""
+    def __init__(self, cursor):
+        global _savepoint_names
+
+        if cursor.connection.isolation_level is not None:
+            msg = ('The cursor\'s connection must be running in '
+                   '"autocommit" mode for precise transaction and '
+                   'savepoint handling. Turn on autocommit by '
+                   'assigning "isolation_level=None".')
+            raise ValueError(msg)
+
+        self.name = next(_savepoint_names)
+        self.cursor = cursor
+
+    def __enter__(self):
+        self.cursor.execute('SAVEPOINT {0}'.format(self.name))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.cursor.execute('RELEASE {0}'.format(self.name))
+        else:
+            self.cursor.execute('ROLLBACK TO {0}'.format(self.name))
+
+
+def load_data(cursor, table, *args):
+    """
+    load_data(cursor, table, columns, records)
+    load_data(cursor, table, records)
+    """
+    try:
+        records, = args
+        columns = None
+    except ValueError:
+        columns, records = args
+
+    records = iter(records)
+    first_record = next(records, None)
+    if columns:
+        if first_record:
+            records = chain([first_record], records)
+    else:
+        if not first_record:
+            return  # <- EXIT! (No table created.)
+        try:  # Try mapping.
+            columns = list(first_record.keys())
+            records = chain([first_record], records)
+        except AttributeError:
+            try:  # Try namedtuple.
+                columns = first_record._fields
+                records = chain([first_record], records)
+            except AttributeError:
+                columns = first_record  # Use first row as column names.
+
+    if not isinstance(columns, Iterable) or isinstance(columns, str):
+        msg = 'expected iterable of strings, got {0!r}'
+        raise TypeError(msg.format(columns))
+    columns = list(columns)  # Make sure columns is a sequence.
+
+    if isinstance(first_record, Mapping):
+        records = ([rec.get(c, '') for c in columns] for rec in records)
+
+    with savepoint(cursor):
+        if table_exists(cursor, table):
+            alter_table(cursor, table, columns, default="''")
+        else:
+            create_table(cursor, table, columns, default="''")
+        insert_records(cursor, table, columns, records)
---
+++ tests/test_temptable.py
@@ -0,0 +1,459 @@
+# -*- coding: utf-8 -*-
+import itertools
+import sqlite3
+import unittest
+import collections
+
+import datatest._dataaccess.temptable as temptable
+from datatest._dataaccess.temptable import (
+    table_exists,
+    new_table_name,
+    normalize_names,
+    create_table,
+    get_columns,
+    insert_records,
+    alter_table,
+    drop_table,
+    savepoint,
+    load_data,
+)
+
+
+class TestTableExists(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        self.cursor = connection.cursor()
+
+    def test_empty_database(self):
+        self.assertFalse(table_exists(self.cursor, 'table_a'))
+
+    def test_persistent_table(self):
+        self.cursor.execute('CREATE TABLE table_b (col1, col2)')
+        self.assertTrue(table_exists(self.cursor, 'table_b'))
+
+    def test_temporary_table(self):
+        self.cursor.execute('CREATE TEMPORARY TABLE table_c (col1, col2)')
+        self.assertTrue(table_exists(self.cursor, 'table_c'))
+
+
+class TestNewTableName(unittest.TestCase):
+    def setUp(self):
+        # Rebuild internal generator.
+        temptable._table_names = ('tbl{0}'.format(x) for x in itertools.count())
+
+        connection = sqlite3.connect(':memory:')
+        self.cursor = connection.cursor()
+
+    def test_empty_database(self):
+        table_name = new_table_name(self.cursor)
+        self.assertEqual(table_name, 'tbl0')
+
+    def test_existing_temptable(self):
+        self.cursor.execute('CREATE TEMPORARY TABLE tbl0 (col1, col2)')
+        table_name = new_table_name(self.cursor)
+        self.assertEqual(table_name, 'tbl1')
+
+    def test_existing_table_and_temptable(self):
+        self.cursor.execute('CREATE TABLE tbl0 (col1, col2)')
+        self.cursor.execute('CREATE TEMPORARY TABLE tbl1 (col1, col2)')
+        table_name = new_table_name(self.cursor)
+        self.assertEqual(table_name, 'tbl2')
+
+
+class TestNormalizeNames(unittest.TestCase):
+    def test_single_value(self):
+        normalized = normalize_names('A')
+        self.assertEqual(normalized, '"A"')
+
+    def test_list_of_values(self):
+        normalized = normalize_names(['A', 'B'])
+        expected = ['"A"', '"B"']
+        self.assertEqual(normalized, expected)
+
+    def test_non_strings(self):
+        normalized = normalize_names(2.5)
+        self.assertEqual(normalized, '"2.5"')
+
+    def test_whitespace(self):
+        normalized = normalize_names('  A  ')
+        self.assertEqual(normalized, '"A"')
+
+        normalized = normalize_names('    ')
+        self.assertEqual(normalized, '""')
+
+    def test_quote_escaping(self):
+        normalized = normalize_names('Steve "The Woz" Wozniak')
+        self.assertEqual(normalized, '"Steve ""The Woz"" Wozniak"')
+
+
+class TestCreateTable(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        self.cursor = connection.cursor()
+
+    def count_tables(self):  # <- Heper function.
+        self.cursor.execute('''
+            SELECT COUNT(*)
+            FROM sqlite_temp_master
+            WHERE type='table'
+        ''')
+        return self.cursor.fetchone()[0]
+
+    def test_basic_creation(self):
+        self.assertEqual(self.count_tables(), 0, msg='starting with zero tables')
+
+        create_table(self.cursor, 'test_table1', ['A', 'B'])  # <- Create table!
+        self.assertEqual(self.count_tables(), 1, msg='one table')
+
+        create_table(self.cursor, 'test_table2', ['A', 'B'])  # <- Create table!
+        self.assertEqual(self.count_tables(), 2, msg='two tables')
+
+    def test_default_value(self):
+        # When unspecified, default is empty string.
+        create_table(self.cursor, 'test_table1', ['A', 'B'])
+        self.cursor.execute("INSERT INTO test_table1 (A) VALUES ('foo')")
+        self.cursor.execute("INSERT INTO test_table1 (B) VALUES ('bar')")
+
+        self.cursor.execute('SELECT * FROM test_table1')
+        expected = [
+            ('foo', ''),  # <- Default in column B
+            ('', 'bar'),  # <- Default in column A
+        ]
+        self.assertEqual(self.cursor.fetchall(), expected)
+
+        # Setting default to None.
+        create_table(self.cursor, 'test_table2', ['A', 'B'], default=None)
+        self.cursor.execute("INSERT INTO test_table2 (A) VALUES ('foo')")
+        self.cursor.execute("INSERT INTO test_table2 (B) VALUES ('bar')")
+
+        self.cursor.execute('SELECT * FROM test_table2')
+        expected = [
+            ('foo', None),  # <- Default in column B
+            (None, 'bar'),  # <- Default in column A
+        ]
+        self.assertEqual(self.cursor.fetchall(), expected)
+
+    def test_sqlite3_errors(self):
+        """Sqlite errors should not be caught."""
+        # Table already exists.
+        create_table(self.cursor, 'test_table1', ['A', 'B'])
+        with self.assertRaises(sqlite3.OperationalError):
+            create_table(self.cursor, 'test_table1', ['A', 'B'])
+
+        # Duplicate column name.
+        with self.assertRaises(sqlite3.OperationalError):
+            create_table(self.cursor, 'test_table2', ['A', 'B', 'A'])
+
+        # Duplicate column name (after normalization).
+        with self.assertRaises(sqlite3.OperationalError):
+            create_table(self.cursor, 'test_table3', ['A', 'B', '  A  '])
+
+        # Duplicate empty/all-whitespace string columns (uses modified message).
+        with self.assertRaises(sqlite3.OperationalError) as cm:
+            create_table(self.cursor, 'test_table4', ['', 'B', '    '])
+
+
+class TestGetColumns(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        self.cursor = connection.cursor()
+
+    def test_get_columns(self):
+        self.cursor.execute('CREATE TABLE test1 ("A", "B")')
+        columns = get_columns(self.cursor, 'test1')
+        self.assertEqual(columns, ['A', 'B'])
+
+        self.cursor.execute('CREATE TEMPORARY TABLE test2 ("C", "D")')
+        columns = get_columns(self.cursor, 'test2')
+        self.assertEqual(columns, ['C', 'D'])
+
+    def test_missing_table(self):
+        with self.assertRaises(sqlite3.ProgrammingError):
+            columns = get_columns(self.cursor, 'missing_table')
+
+
+class TestInsertRecords(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        self.cursor = connection.cursor()
+
+    def test_basic_insert(self):
+        cursor = self.cursor
+
+        cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        records = [
+            ('x', 1),
+            ('y', 2),
+        ]
+        insert_records(cursor, 'test_table', ['A', 'B'], records)
+
+        cursor.execute('SELECT * FROM test_table')
+        results = cursor.fetchall()
+
+        self.assertEqual(results, records)
+
+    def test_reordered_columns(self):
+        cursor = self.cursor
+
+        cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        records = [
+            (1, 'x'),
+            (2, 'y'),
+        ]
+        columns = ['B', 'A']  # <- Column order doesn't match how table was created.
+        insert_records(cursor, 'test_table', columns, records)
+
+        cursor.execute('SELECT * FROM test_table')
+        results = cursor.fetchall()
+
+        expected = [
+            ('x', 1),
+            ('y', 2),
+        ]
+        self.assertEqual(results, expected)
+
+    def test_wrong_number_of_values(self):
+        self.cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+
+        too_few = [('x',), ('y',)]
+        with self.assertRaises(sqlite3.ProgrammingError):
+            insert_records(self.cursor, 'test_table', ['A', 'B'], too_few)
+
+        too_many = [('x', 1, 'foo'), ('y', 2, 'bar')]
+        with self.assertRaises(sqlite3.ProgrammingError):
+            insert_records(self.cursor, 'test_table', ['A', 'B'], too_many)
+
+    def test_no_records(self):
+        cursor = self.cursor
+
+        cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        records = iter([])  # <- Empty, no data.
+        insert_records(cursor, 'test_table', ['A', 'B'], records)
+
+        cursor.execute('SELECT * FROM test_table')
+        results = cursor.fetchall()
+
+        self.assertEqual(results, [])
+
+    def test_sqlite3_errors(self):
+        """Sqlite errors should not be caught."""
+        # No such table.
+        with self.assertRaises(sqlite3.OperationalError):
+            records = [('x', 1), ('y', 2)]
+            insert_records(self.cursor, 'missing_table', ['A', 'B'], records)
+
+        # No column named X.
+        with self.assertRaises(sqlite3.OperationalError):
+            self.cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+            records = [('a', 1), ('b', 2)]
+            insert_records(self.cursor, 'test_table', ['X', 'B'], records)
+
+
+class TestAlterTable(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        self.cursor = connection.cursor()
+
+    def test_new_columns(self):
+        self.cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        alter_table(self.cursor, 'test_table', ['C', 'D'])
+
+        columns = get_columns(self.cursor, 'test_table')
+        self.assertEqual(columns, ['A', 'B', 'C', 'D'])
+
+    def test_existing_columns(self):
+        self.cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        alter_table(self.cursor, 'test_table', ['A', 'B', 'C', 'D'])
+
+        columns = get_columns(self.cursor, 'test_table')
+        self.assertEqual(columns, ['A', 'B', 'C', 'D'])
+
+    def test_ordering_behavior(self):
+        self.cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        alter_table(self.cursor, 'test_table', ['B', 'C', 'A', 'D'])
+
+        # Columns A and B already exist in a specified order and
+        # the new columns ('C' and 'D') are added in the order in
+        # which they are encountered.
+        columns = get_columns(self.cursor, 'test_table')
+        self.assertEqual(columns, ['A', 'B', 'C', 'D'])
+
+
+class TestDropTable(unittest.TestCase):
+    def test_drop_table(self):
+        connection = sqlite3.connect(':memory:')
+        cursor = connection.cursor()
+
+        cursor.execute('CREATE TEMPORARY TABLE test_table ("A", "B")')
+        self.assertTrue(table_exists(cursor, 'test_table'))
+
+        drop_table(cursor, 'test_table')  # <- Drop table!
+        self.assertFalse(table_exists(cursor, 'test_table'))
+
+
+class TestSavepoint(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        connection.isolation_level = None
+        self.cursor = connection.cursor()
+
+    def test_transaction_status(self):
+        connection = self.cursor.connection
+        if not hasattr(connection, 'in_transaction'):  # New in 3.2.
+            return
+
+        self.assertFalse(connection.in_transaction)
+        with savepoint(self.cursor):
+            self.assertTrue(connection.in_transaction)
+        self.assertFalse(connection.in_transaction)
+
+    def test_release(self):
+        cursor = self.cursor
+
+        with savepoint(cursor):
+            cursor.execute('CREATE TEMPORARY TABLE test_table ("A")')
+            cursor.execute("INSERT INTO test_table VALUES ('one')")
+            cursor.execute("INSERT INTO test_table VALUES ('two')")
+            cursor.execute("INSERT INTO test_table VALUES ('three')")
+
+        cursor.execute('SELECT * FROM test_table')
+        self.assertEqual(cursor.fetchall(), [('one',), ('two',), ('three',)])
+
+    def test_nested_releases(self):
+        cursor = self.cursor
+
+        with savepoint(cursor):
+            cursor.execute('CREATE TEMPORARY TABLE test_table ("A")')
+            cursor.execute("INSERT INTO test_table VALUES ('one')")
+            with savepoint(cursor):  # <- Nested!
+                cursor.execute("INSERT INTO test_table VALUES ('two')")
+            cursor.execute("INSERT INTO test_table VALUES ('three')")
+
+        cursor.execute('SELECT * FROM test_table')
+        self.assertEqual(cursor.fetchall(), [('one',), ('two',), ('three',)])
+
+    def test_rollback(self):
+        cursor = self.cursor
+
+        with savepoint(cursor):  # <- Released.
+            cursor.execute('CREATE TEMPORARY TABLE test_table ("A")')
+
+        try:
+            with savepoint(cursor):  # <- Rolled back!
+                cursor.execute("INSERT INTO test_table VALUES ('one')")
+                cursor.execute("INSERT INTO test_table VALUES ('two')")
+                cursor.execute("INSERT INTO missing_table VALUES ('three')")  # <- Bad table.
+        except sqlite3.OperationalError:
+            pass
+
+        cursor.execute('SELECT * FROM test_table')
+        self.assertEqual(cursor.fetchall(), [], 'Table should exist but contain no records.')
+
+    def test_nested_rollback(self):
+        cursor = self.cursor
+
+        with savepoint(cursor):  # <- Released.
+            cursor.execute('CREATE TEMPORARY TABLE test_table ("A")')
+            cursor.execute("INSERT INTO test_table VALUES ('one')")
+            try:
+                with savepoint(cursor):  # <- Nested rollback!
+                    cursor.execute("INSERT INTO test_table VALUES ('two')")
+                    raise Exception()
+            except Exception:
+                pass
+            cursor.execute("INSERT INTO test_table VALUES ('three')")
+
+        cursor.execute('SELECT * FROM test_table')
+        self.assertEqual(cursor.fetchall(), [('one',), ('three',)])
+
+    def test_bad_isolation_level(self):
+        connection = sqlite3.connect(':memory:')
+        connection.isolation_level = 'DEFERRED'  # <- Expects None/autocommit!
+        cursor = connection.cursor()
+
+        with self.assertRaises(ValueError):
+            with savepoint(cursor):
+                pass
+
+
+class TestLoadData(unittest.TestCase):
+    def setUp(self):
+        connection = sqlite3.connect(':memory:')
+        connection.isolation_level = None
+        self.cursor = connection.cursor()
+
+        try:
+            self.dict_constructor = collections.OrderedDict  # New in 2.7
+        except AttributeError:
+            self.dict_constructor = dict
+
+    def test_four_args(self):
+        columns = ['A', 'B']
+        records = [
+            ('x', 1),
+            ('y', 2),
+        ]
+        load_data(self.cursor, 'testtable', columns, records)  # <- Four args.
+        self.cursor.execute('SELECT A, B FROM testtable')
+        self.assertEqual(self.cursor.fetchall(), [('x', 1), ('y', 2)])
+
+    def test_four_args_mappings(self):
+        columns = ['A', 'B']
+        records = [
+            self.dict_constructor([('A', 'x'), ('B', 1)]),
+            self.dict_constructor([('B', 2), ('A', 'y')]),  # <- Different key order.
+        ]
+        load_data(self.cursor, 'testtable', columns, records)  # <- Four args.
+        self.cursor.execute('SELECT A, B FROM testtable')
+        self.assertEqual(self.cursor.fetchall(), [('x', 1), ('y', 2)])
+
+    def test_three_args(self):
+        records = [
+            ['A', 'B'],  # <- Used as header row.
+            ('x', 1),
+            ('y', 2),
+        ]
+        load_data(self.cursor, 'testtable', records)  # <- Three args.
+        self.cursor.execute('SELECT A, B FROM testtable')
+        self.assertEqual(self.cursor.fetchall(), [('x', 1), ('y', 2)])
+
+    def test_three_args_mappings(self):
+        records = [
+            self.dict_constructor([('A', 'x'), ('B', 1)]),
+            self.dict_constructor([('B', 2), ('A', 'y')]),  # <- Different key order.
+        ]
+        load_data(self.cursor, 'testtable', records)  # <- Three args.
+        self.cursor.execute('SELECT A, B FROM testtable')
+        self.assertEqual(self.cursor.fetchall(), [('x', 1), ('y', 2)])
+
+    def test_three_args_namedtuples(self):
+        ntup = collections.namedtuple('ntup', ['A', 'B'])
+        records = [
+            ntup('x', 1),
+            ntup('y', 2),
+        ]
+        load_data(self.cursor, 'testtable', records)  # <- Three args.
+        self.cursor.execute('SELECT A, B FROM testtable')
+        self.assertEqual(self.cursor.fetchall(), [('x', 1), ('y', 2)])
+
+    def test_empty_records(self):
+        records = []
+
+        load_data(self.cursor, 'testtable1', ['A', 'B'], records)  # <- Using four args.
+        self.assertTrue(table_exists(self.cursor, 'testtable1'), 'should create table')
+        self.cursor.execute('SELECT A, B FROM testtable1')
+        self.assertEqual(self.cursor.fetchall(), [], 'should have zero records')
+
+        load_data(self.cursor, 'testtable2', records)  # <- Using three args.
+        self.assertFalse(table_exists(self.cursor, 'testtable2'), 'should not create table')
+
+    def test_bad_columns_object(self):
+        records = [('x', 1), ('y', 2)]
+        columns = 'bad columns object'  # <- Expects iterable of names, not this str.
+
+        with self.assertRaises(TypeError):
+            load_data(self.cursor, 'testtable', columns, records)
+
+
+if __name__ == '__main__':
+    unittest.main()
