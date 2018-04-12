8db4020fdcda8baa56c0e105e82e1696a23fe3b3
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-04-11 22:09:22 -0400

    Add tests for idiomatic use cases that we want to support or at least not change without consideration.

---
+++ tests/test_usecases.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""A handful of integration tests to check for idiomatic use cases
+that we want make sure are as convinient as possible for users.
+"""
+from . import _unittest as unittest
+import datatest
+
+
+class TestSelectorIdioms(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.selector_a = datatest.Selector([
+            ['A', 'B', 'C'],
+            ['x', 1, 100],
+            ['y', 2, 200],
+            ['z', 3, 300],
+        ])
+        cls.selector_b = datatest.Selector([
+            ['A', 'B'],
+            ['x', 1],
+            ['y', 2],
+            ['z', 3],
+        ])
+
+    def test_compare_fieldnames(self):
+        """Should be able to compare ``fieldnames`` between Selectors
+        by simply casting the *requirement* as a set and comparing it
+        directly against the ``fieldnames`` parameter of the other
+        Selector.
+        """
+        a = self.selector_a
+        b = self.selector_b
+
+        # A case we want to optimize.
+        datatest.validate(a.fieldnames, set(a.fieldnames))
+
+        # A case we want to optimize.
+        with datatest.allowed.specific(datatest.Extra('C')):
+            datatest.validate(a.fieldnames, set(b.fieldnames))
+
+    def test_compare_rows(self):
+        """Should be able to compare rows by calling a selector by
+        its own fieldnames.
+        """
+        a = self.selector_a
+        b = self.selector_b
+
+        # A case we want to optimize.
+        datatest.validate(a(a.fieldnames), a(a.fieldnames))
+
+        # A case we want to optimize (using ordered intersection of fieldnames).
+        common_fields = tuple(x for x in a.fieldnames if x in b.fieldnames)
+        datatest.validate(a(common_fields), b(common_fields))
