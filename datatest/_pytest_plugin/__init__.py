5ad76f3171f159d678cf271625cd833fdd031c5a
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-05-25 09:15:40 -0400

    Renamed the bundled pytest plugin from pytest_datatest to _pytest_datatest.
    With the release of Pytest 3.6.0, pluggy--a pytest dependency
    that was recently updated--has been raising the following
    error:
    
      pluggy.PluginValidationError: unknown hook 'pytest_datatest'
      in plugin <module 'datatest._pytest_plugin'
    
    The related behavior was changed in Pytest 3.6.0 in response
    to a deprecation in pluggy (pytest-dev/pytest#3487). While
    it seems clear that pluggy supported this without problems
    (see pytest-dev/pluggy#91), I'm not sure if the latest Pytest
    behavior is intentional or a regression. But in either case,
    we want to make sure the above error is avoided.

--- datatest/_pytest_plugin/__init__.py
+++ datatest/_pytest_plugin/__init__.py
@@ -4,14 +4,14 @@ from __future__ import absolute_import
 try:
     # Check if the external development version is installed.
     import pytest_datatest as _
-    from .pytest_datatest import version
-    from .pytest_datatest import version_info
+    from ._pytest_datatest import version
+    from ._pytest_datatest import version_info
 except ImportError:
     try:
         # If there's no external plugin, load the bundled version.
-        from .pytest_datatest import pytest_runtest_makereport
-        from .pytest_datatest import version
-        from .pytest_datatest import version_info
+        from ._pytest_datatest import pytest_runtest_makereport
+        from ._pytest_datatest import version
+        from ._pytest_datatest import version_info
     except ImportError:
         # If there's no Pytest support at all, set dummy version numbers.
         version = '0.0.0'
