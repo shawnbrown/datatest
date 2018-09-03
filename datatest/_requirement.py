9014ecee72f5b6f0fc341cd9b458830e9461de13
Shawn Brown <shawnbrown@users.noreply.github.com>
2018-09-03 19:43:00 -0400

    Add check to verify that filterfalse() always returns an iterable of difference objects.

--- datatest/_requirement.py
+++ datatest/_requirement.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from ._compatibility.builtins import *
 from ._compatibility import abc
+from ._compatibility.collections.abc import Iterable
+from .difference import BaseDifference
 from ._utils import iterpeek
+from ._utils import nonstringiter
 
 
 class Requirement(abc.ABC):
@@ -12,11 +15,34 @@ class Requirement(abc.ABC):
 
     @abc.abstractmethod
     def filterfalse(self, iterable):
+        """Return a non-string iterable of differences for values in
+        *iterable* that do not satisfy the requirement.
+        """
         raise NotImplementedError
 
+    @staticmethod
+    def _verify_filterfalse(iterable):
+        """Verify that filterfalse() returns a non-string iterable of
+        differences.
+        """
+        if not nonstringiter(iterable):
+            cls_name = iterable.__class__.__name__
+            message = ('filterfalse() must return non-string iterable, '
+                       'got {0!r} instead')
+            raise TypeError(message.format(cls_name))
+
+        for value in iterable:
+            if not isinstance(value, BaseDifference):
+                cls_name = value.__class__.__name__
+                message = ('filterfalse() result must contain difference '
+                           'objects, got {0}: {1!r}')
+                raise TypeError(message.format(cls_name, value))
+            yield value
+
     def __call__(self, iterable):
-        diffs = self.filterfalse(iterable)
-        first_element, diffs = iterpeek(diffs)
+        filtered = self.filterfalse(iterable)
+        normalized = self._verify_filterfalse(filtered)
+        first_element, normalized = iterpeek(normalized)
         if first_element:
-            return diffs
+            return normalized
         return None
--- tests/test_requirement.py
+++ tests/test_requirement.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from . import _unittest as unittest
+from datatest import Invalid
 from datatest._requirement import Requirement
 
 
@@ -10,8 +11,32 @@ class TestRequirement(unittest.TestCase):
         class IncompleteSubclass(Requirement):
             pass
 
-        with self.assertRaises(TypeError):
+        regex = "Can't instantiate abstract class"
+        with self.assertRaisesRegex(TypeError, regex):
             requirement = IncompleteSubclass()
+
+    def test_bad_filterfalse(self):
+        """Should raise error if filterfalse() does not return an iterable
+        of differences.
+        """
+        class BadFilterfalse(Requirement):
+            @property
+            def msg(self):
+                return 'requirement message'
+
+            def filterfalse(self, iterable):
+                return [
+                    Invalid('abc'),
+                    Invalid('def'),
+                    'ghi',  # <- Not a difference object!
+                ]
+
+        requirement = BadFilterfalse()
+
+        regex = 'must contain difference objects'
+        with self.assertRaisesRegex(TypeError, regex):
+            result = requirement([])
+            list(result)
 
     def test_simple_subclass(self):
         """Test basic subclass behavior."""
@@ -24,12 +49,12 @@ class TestRequirement(unittest.TestCase):
                 return 'require {0!r}'.format(self.value)
 
             def filterfalse(self, iterable):
-                return (x for x in iterable if x != self.value)
+                return (Invalid(x) for x in iterable if x != self.value)
 
         requirement = RequiredValue('abc')
 
         result = requirement(['abc', 'def', 'ghi'])
-        self.assertEqual(list(result), ['def', 'ghi'])
+        self.assertEqual(list(result), [Invalid('def'), Invalid('ghi')])
 
         result = requirement(['abc', 'abc', 'abc'])
         self.assertIsNone(result)
