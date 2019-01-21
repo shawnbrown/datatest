dc024aee55d3453ebaf7dcf20fb64dcf5b39836c
Shawn Brown <shawnbrown@users.noreply.github.com>
2019-01-21 18:39:41 -0500

    Add patch to allow line-wrapping for sphinx_rtd_theme tables.
    This solution is adapted from ideas discussed on the following
    issue:
    
        https://github.com/rtfd/sphinx_rtd_theme/issues/117

--- docs/conf.py
+++ docs/conf.py
@@ -129,6 +129,9 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme = 'scrolls'
 #html_theme = 'bizstyle'
 
+def setup(app):
+    app.add_stylesheet('theme_overrides.css')
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
