2e079c2047835d12cd0399ac57b64d55255e1b5d
Shawn Brown <shawnbrown@users.noreply.github.com>
2019-01-29 11:24:57 -0500

    Update bundled pytest-datatest plugin to version 0.1.3.

--- datatest/_pytest_plugin.py
+++ datatest/_pytest_plugin.py
@@ -42,8 +42,8 @@ else:
     _bundled_version_info = (0, 0, 0)
 
 
-version = '0.1.2'
-version_info = (0, 1, 2)
+version = '0.1.3'
+version_info = (0, 1, 3)
 
 _idconfig_session_dict = {}  # Dictionary to store ``session`` reference.
 
@@ -64,7 +64,8 @@ def pytest_addoption(parser):
             ),
         )
     except ValueError as exc:
-        assert 'already added' in str(exc)
+        if 'already added' not in str(exc):
+            raise
 
 
 def pytest_plugin_registered(plugin, manager):
@@ -90,8 +91,8 @@ def pytest_collection_modifyitems(session, config, items):
 
 # Compile regex patterns to match error message text.
 _diff_start_regex = re.compile(
-    '^E\s+(?:datatest.)?ValidationError:.+\d+ difference[s]?.*: [\[{]$')
-_diff_stop_regex = re.compile('^E\s+(?:\}|\]|\.\.\.)$')
+    r'^E\s+(?:datatest.)?ValidationError:.+\d+ difference[s]?.*: [\[{]$')
+_diff_stop_regex = re.compile(r'^E\s+(?:\}|\]|\.\.\.)$')
 
 
 class DatatestReprEntry(ReprEntry):
@@ -206,10 +207,18 @@ def pytest_runtest_makereport(item, call):
             result.longrepr.reprtraceback.reprentries = new_entries
 
         # If test was mandatory, session should fail immediately.
-        if (call.excinfo and item.get_marker('mandatory')
-                and not item.config.getoption('--ignore-mandatory')):
-            shouldfail = 'mandatory {0!r} failed'.format(item.name)
-            item.session.shouldfail = shouldfail
+        if call.excinfo:
+            try:
+                mandatory = item.get_closest_marker('mandatory')
+            except AttributeError:
+                try:
+                    mandatory = item.get_marker('mandatory')  # pytest <= 3.5
+                except AttributeError:
+                    mandatory = False  # in pytest <= 3.6 item can be non-Item
+
+            if mandatory and not item.config.getoption('--ignore-mandatory'):
+                shouldfail = 'mandatory {0!r} failed'.format(item.name)
+                item.session.shouldfail = shouldfail
 
     else:
         outcome = yield
