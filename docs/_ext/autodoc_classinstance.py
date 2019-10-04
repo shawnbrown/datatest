79e1ed0c2d8ce56ce85d85403063c879db42072e
Shawn Brown <shawnbrown@users.noreply.github.com>
2019-10-04 01:25:30 -0400

    Update autodoc_classinstance plugin to be compatible with Sphinx 2.x.

--- docs/_ext/autodoc_classinstance.py
+++ docs/_ext/autodoc_classinstance.py
@@ -3,11 +3,8 @@
 """Sphinx Extension: autodoc_classinstance (written by Shawn Brown)."""
 from sphinx.domains.python import PyClasslike
 from sphinx.ext.autodoc import ClassDocumenter
-from sphinx.ext.autodoc import ClassLevelDocumenter
 from sphinx.ext.autodoc import MethodDocumenter
-from sphinx.util.inspect import isclassmethod
-from sphinx.util.inspect import isstaticmethod
-from sphinx.util.inspect import Signature
+from sphinx.util import inspect
 
 
 class PyClassInstance(PyClasslike):
@@ -26,58 +23,44 @@ class ClassInstanceDocumenter(ClassDocumenter):
 
     @classmethod
     def can_document_member(cls, member, membername, isattr, parent):
-        # type: (Any, str, bool, Any) -> bool
         return not isinstance(member, type)
 
     def import_object(self):
-        # type: () -> Any
         ret = super().import_object()
         self.doc_as_attr = False  # never document as a data/attribute
         return ret
 
     def format_args(self):
-        # type: () -> str
         # for instances, the relevant signature is the __call__ method's
         callmeth = self.get_attr(self.object, '__call__', None)
         if callmeth:
-            return Signature(callmeth, bound_method=True, has_retval=True).format_args()
+            sig = inspect.Signature(callmeth, bound_method=True, has_retval=True)
+            return sig.format_args()
         return None
 
 
 class AlternateMethodDocumenter(MethodDocumenter):
     """
-    Alternative specialized Documenter subclass for methods (normal,
-    static and class) of classes and class isntances.
+    Alternative documenter for methods of classes and class instances.
     """
-    objtype = 'method'
+    def add_directive_header(self, sig):
+        if not isinstance(self.parent, type):  # If class instance.
+            # Call grandparent method.
+            super(MethodDocumenter, self).add_directive_header(sig)
 
-    def import_object(self):
-        # type: () -> Any
-        ret = ClassLevelDocumenter.import_object(self)
-        if not ret:
-            return ret
+            # Tag as "async" if appropriate.
+            sourcename = self.get_sourcename()
+            parentclass = self.parent.__class__
+            obj = parentclass.__dict__.get(self.object_name, self.object)
+            if inspect.iscoroutinefunction(obj):
+                self.add_line('   :async:', sourcename)
 
-        # get parent's class
-        parent_cls = self.parent
-        if not isinstance(parent_cls, type):
-            parent_cls = parent_cls.__class__  # if instance, get its class
+            # Note: The method types like abstractmethod, classmethod,
+            # and instancemethod are NOT tagged as such on instances.
 
-        # to distinguish classmethod/staticmethod
-        obj = parent_cls.__dict__.get(self.object_name)
-        if obj is None:
-            obj = self.object
-
-        if isclassmethod(obj):
-            self.directivetype = 'classmethod'
-            # document class and static members before ordinary ones
-            self.member_order = self.member_order - 1
-        elif isstaticmethod(obj, cls=parent_cls, name=self.object_name):
-            self.directivetype = 'staticmethod'
-            # document class and static members before ordinary ones
-            self.member_order = self.member_order - 1
-        else:
-            self.directivetype = 'method'
-        return ret
+        else:  # Else normal class.
+            # Call parent method.
+            super(AlternateMethodDocumenter, self).add_directive_header(sig)
 
 
 def setup(app):
